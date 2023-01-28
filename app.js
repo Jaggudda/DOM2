fc6d431fecef143bd43a004c1842d012cf4f0bc2
prahalad <jayeshkumar171@gmail.com>
2023-01-28 21:45:31 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+/*const title = document.getElementById('header-title');
+const header = document.getElementById('main-header');
+const mains = document.getElementById('main');
+header.style.borderBottom = 'solid 3px #000';
+header.style.fontWeight = 'bold';*/
+console.log("document12");
