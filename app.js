8bd6a62aaa5b3c6ee87505300035a936acc2402e
prahalad <jayeshkumar171@gmail.com>
2023-01-29 16:43:56 +0530

    feat: add new Li element

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ console.log("domain");
 
 var item3 = document.getElementById("items").getElementsByTagName("li")[2];
 item3.style.color = "green";
+
+var ul = document.getElementById("items");
+var li = document.createElement("li");
+li.className = "list-group-item";
+li.innerHTML = "Item 5";
+ul.appendChild(li);
