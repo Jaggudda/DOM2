4446c5b154e0d6c00117434c50c7399de2093590
prahalad <jayeshkumar171@gmail.com>
2023-01-29 17:00:14 +0530

    feat: add item 3

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 console.log("domain");
 
 
-var item3 = document.getElementById("items").getElementsByTagName("li")[2];
-item3.style.color = "green";
+//var item3 = document.getElementById("items").getElementsByTagName("li")[2];
+//item3.style.color = "green";
 
 var ul = document.getElementById("items");
 var li = document.createElement("li");
 li.className = "list-group-item";
-li.innerHTML = "Item 5";
 ul.appendChild(li);
+
+var item2 = document.getElementById("items").getElementsByTagName("li")[1];
+item2.style.backgroundColor = "green";
+
+
+let item3 = document.getElementsByClassName("list-group-item")[2];
+item3.style.display = "none";
